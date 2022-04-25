8e56ae60081b94c39af25d6632c1a329ac77ab8a
peterturkson102 <peterturkson102@gmail.com>
2022-04-25 00:07:46 +0000

    made some cosmetic changes

--- src/Mains.java
+++ src/Mains.java
@@ -106,27 +106,27 @@ public static class GFG {
         // Adjacency list representation of the
         // connected edges by declaring List class object
         // Declaring object of type List<Node>
-        List<List<Node> > adj
-                = new ArrayList<List<Node> >();
+        List<List<Node> > obj
+                = new ArrayList<>();
 
         // Initialize list for every node
         for (int i = 0; i < V; i++) {
-            List<Node> item = new ArrayList<Node>();
-            adj.add(item);
+            List<Node> item = new ArrayList<>();
+            obj.add(item);
         }
 
         // Inputs for the GFG(dpq) graph
-        adj.get(0).add(new Node(1, 9));
-        adj.get(0).add(new Node(2, 6));
-        adj.get(0).add(new Node(3, 5));
-        adj.get(0).add(new Node(4, 3));
+        obj.get(0).add(new Node(1, 9));
+        obj.get(0).add(new Node(2, 6));
+        obj.get(0).add(new Node(3, 5));
+        obj.get(0).add(new Node(4, 3));
 
-        adj.get(2).add(new Node(1, 2));
-        adj.get(2).add(new Node(3, 4));
+        obj.get(2).add(new Node(1, 2));
+        obj.get(2).add(new Node(3, 4));
 
         // Calculating the single source shortest path
         GFG dpq = new GFG(V);
-        dpq.dijkstra(adj, source);
+        dpq.dijkstra(obj, source);
 
         // Printing the shortest path to all the nodes
         // from the source node
